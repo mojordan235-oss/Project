99b8e862b13d00a87dc024505a7f8e74321a531f
mojordan235-oss <mojordan235@gmail.com>
2025-09-24 16:03:57 -0400

    Milestone #3

---
+++ Milestine #3.R
@@ -0,0 +1,15 @@
+#1.hist(movies$Gross)
+#2.hist(movies$Gross, main="Movie Gross", xlab="Gross",ylab="Frequencey")
+#3.In the histogram when the gross increase. And when the frequecnecy decrase and also when gross decrase and the frequcency increase.
+
+#1.hist(movies$Gross);
+#2.hist(movies$Gross, main="Movie Gross", xlab="Gross",ylab="Frequencey")
+#3.In the histogram when the number of voters watch the movies increase. And when the frequecnecy decrase and also when the number of voters watch the movies decrase and the frequcency increase.
+
+#1.barplot(movies$IMDB_Rating)
+#2.barplot(movies$IMDB_Rating, main="Movie Rating", xlab="Rating", ylab="Results")
+#3.The thing I found suprsing about the barplot of the rating is that there no x axis. And the graph is decrasing form left to right.
+
+#1.barplot(movies$Meta_score)
+#2.barplot(movies$Meta_score, main="Meta Scores", xlab="Scores", ylab="Results")
+#3..The thing I found suprsing about the barplot of the rating is that there no x axis. And the graph is incrasing and decrasing depending the results.
