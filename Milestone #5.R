a3f179c52cbe25f244583c53ccb0addfcdbf4a08
mojordan235-oss <mojordan235@gmail.com>
2025-10-23 11:30:37 -0400

    Add files via upload

---
+++ Milestone #5.R
@@ -0,0 +1,16 @@
+mean(movies$IMDB_Rating)
+[1] 7.9493
+median(movies$IMDB_Rating)
+[1] 7.9
+var(movies$IMDB_Rating)
+[1] 0.07589541
+sd(movies$IMDB_Rating)
+[1] 0.2754912
+mean(movies$No_of_Votes)
+[1] 273692.9
+median(movies$No_of_Votes)
+[1] 138548.5
+sd(movies$No_of_Votes)
+[1] 327372.7
+var(movies$No_of_Votes)
+[1] 107172887281
