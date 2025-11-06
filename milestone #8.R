dbc28abd0f9f27e28c70fc08f606dce9831ea166
mojordan235-oss <mojordan235@gmail.com>
2025-11-06 07:05:11 -0500

    Add files via upload

---
+++ milestone #8.R
@@ -0,0 +1,16 @@
+data:  movies$IMDB_Rating
+t = 912.48, df = 999, p-value < 2.2e-16
+alternative hypothesis: true mean is not equal to 0
+95 percent confidence interval:
+  7.932204 7.966396
+sample estimates:
+  mean of x 
+7.9493 
+data:  movies$Gross
+t = 17.87, df = 830, p-value < 2.2e-16
+alternative hypothesis: true mean is not equal to 0
+95 percent confidence interval:
+  60561905 75507597
+sample estimates:
+  mean of x 
+68034751 
