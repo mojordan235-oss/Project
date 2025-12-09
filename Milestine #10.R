eea4c6334d05cedd924408f5a3b71bfa7cfba537
mojordan235-oss <mojordan235@gmail.com>
2025-12-09 11:57:43 -0500

    Add files via upload

---
+++ Milestine #10.R
@@ -0,0 +1,16 @@
+
+> mean(movies$No_of_Votes)
+[1] 273692.9
+> plot(movies$No_of_Votes)
+> t.test(movies$No_of_Votes)
+
+One Sample t-test
+
+data:  movies$No_of_Votes
+t = 26.438, df = 999, p-value < 2.2e-16
+alternative hypothesis: true mean is not equal to 0
+95 percent confidence interval:
+  253377.9 294007.9
+sample estimates:
+  mean of x 
+273692.9 273692.9 
